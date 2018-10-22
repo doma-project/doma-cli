c03fdb04584447dcefea2985c6b3dbd16b7671e5
boofmaq <boofmaq@gmail.com>
2018-10-22 16:45:18 +0500

    Closes #2

--- src/app.cpp
+++ src/app.cpp
@@ -2,18 +2,23 @@
 
 #include <iostream>
 
-#include "screen/EmptyScreen.hpp"
-#include "screen/Screen1_1.hpp"
-
 namespace doma {
 namespace cli {
 
-App::App() : needCloseApp_(false), screen1_1_(), emptyScreen_(), currentScreen_(&screen1_1_) {
+App::App() : needCloseApp_(false), screen0_(), screen1_1_(), emptyScreen_(), currentScreen_(&screen1_1_) {
+  screen0_.registerActionHandler('y', [this]() {
+    save();
+    needCloseApp_ = true;
+  });
+  screen0_.registerActionHandler('n', [this]() {
+    needCloseApp_ = true;
+  });
+
   screen1_1_.registerActionHandler('1', [this]() {
     currentScreen_ = &emptyScreen_;
   });
   screen1_1_.registerActionHandler('q', [this]() {
-    needCloseApp_ = true;
+    currentScreen_ = &screen0_;
   });
 
   emptyScreen_.registerActionHandler('q', [this]() {
@@ -30,6 +35,11 @@ int App::run() {
   } while (!needCloseApp_);
 
   return 0;
+}
+
+void App::save() {
+  /* TODO #4 */
+  std::cout << "Saved successfully!" << std::endl;
 }
 
 } // namespace cli
